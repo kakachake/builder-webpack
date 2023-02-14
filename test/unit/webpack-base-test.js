180d93966c084c2acedc33b979f870c64bc919e8
卡卡查克 <282281884@qq.com>
2023-02-14 21:23:42 +0800

    ci

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -4,10 +4,10 @@ describe("webpack.base.js test case", () => {
   const baseConfig = require("../../lib/webpack.base.js");
   it("entry", () => {
     expect(baseConfig.entry.index.import).to.equal(
-      "E:/frontEnd/wjwebpack/webpackMaintainableConfig/builder-webpack/test/smoke/template/src/index.jsx"
+      "/home/runner/work/builder-webpack/builder-webpack/test/smoke/template/src/index.jsx"
     );
     expect(baseConfig.entry.search.import).to.equal(
-      "E:/frontEnd/wjwebpack/webpackMaintainableConfig/builder-webpack/test/smoke/template/src/search/index.js"
+      "/home/runner/work/builder-webpack/builder-webpack/test/smoke/template/src/search/index.js"
     );
   });
 });
