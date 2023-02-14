1fc1311a498c2c8fc22e873a51e05a932195b91a
卡卡查克 <282281884@qq.com>
2023-02-14 21:25:24 +0800

    ci

--- test/unit/webpack-base-test.js
+++ test/unit/webpack-base-test.js
@@ -3,11 +3,9 @@ const { expect } = require("chai");
 describe("webpack.base.js test case", () => {
   const baseConfig = require("../../lib/webpack.base.js");
   it("entry", () => {
-    expect(baseConfig.entry.index.import).to.equal(
-      "/home/runner/work/builder-webpack/builder-webpack/test/smoke/template/src/index.jsx"
-    );
-    expect(baseConfig.entry.search.import).to.equal(
-      "/home/runner/work/builder-webpack/builder-webpack/test/smoke/template/src/search/index.js"
+    expect(baseConfig.entry.index.import).include("template/src/index.jsx");
+    expect(baseConfig.entry.search.import).include(
+      "template/src/search/index.js"
     );
   });
 });
